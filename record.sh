f0e426b4013e769a46312eb5f61151ac5941d210
bazarum <juantomas.garcia@gmail.com>
2018-03-01 07:49:26 +0100

    new record.sh

--- record.sh
+++ record.sh
@@ -5,13 +5,14 @@ do
  ts=`date +"%s"`
  n_image="$1.image.${ts}.jpeg"
  n_screen="$1.screen.${ts}.jpeg"
- echo "Creating the images ..."
- imagesnap -w 1 $tmpdir/$n_image
+ echo "Creating the images Smile!!! "
+ imagesnap -w 3 $tmpdir/$n_image
+ echo "Done"
  screencapture -x $tmpdir/$n_screen
  echo "Sending to Google Cloud Storage ..."
  gsutil cp $tmpdir/$n_image gs://talksentiment
  gsutil cp $tmpdir/$n_screen gs://talksentimentscreens
  echo "Waiting for the next Status ..."
- sleep 5
+ sleep 2
 done
 
